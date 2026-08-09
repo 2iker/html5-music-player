f2be45e711ae883794ad7711b85fff5f94a63ae7
2iker <2iker@users.noreply.github.com>
2026-08-09 19:09:47 +0800

    Fix: 隐藏滚动条，点击歌曲时更新信息

--- script.js
+++ script.js
@@ -86,25 +86,39 @@ async function searchMusic() {
             // 记录当前播放索引
             var currentPlayIndex = 0;
 
+            // 更新歌曲信息的函数
+            function updateCurrentSongInfo() {
+                var index = player.playIndex;
+                if (index >= 0 && index < songsList.length) {
+                    var song = songsList[index];
+                    var artists = song.ar ? song.ar.map(function(a) { return a.name; }).join('/') : '未知';
+                    var audioUrl = 'https://music.163.com/song/media/outer/url?id=' + song.id + '.mp3';
+                    
+                    $('#j-link').val('https://music.163.com/#/song?id=' + song.id);
+                    $('#j-link-btn').attr('href', 'https://music.163.com/#/song?id=' + song.id);
+                    $('#j-src').val(audioUrl);
+                    $('#j-src-btn').attr('href', audioUrl);
+                    $('#j-songid').val(song.id);
+                    $('#j-name').val(song.name);
+                    $('#j-author').val(artists);
+                }
+            }
+
+            // 监听播放列表点击
+            $('#j-player').on('click', '.aplayer-list li', function() {
+                setTimeout(updateCurrentSongInfo, 100);
+            });
+
             // 监听歌曲切换
+            player.on('play', function() {
+                updateCurrentSongInfo();
+            });
+
             player.on('timeupdate', function() {
                 var newIndex = player.playIndex;
                 if (newIndex !== currentPlayIndex) {
                     currentPlayIndex = newIndex;
-                    if (newIndex >= 0 && newIndex < songsList.length) {
-                        var song = songsList[newIndex];
-                        var artists = song.ar ? song.ar.map(function(a) { return a.name; }).join('/') : '未知';
-                        var audioUrl = 'https://music.163.com/song/media/outer/url?id=' + song.id + '.mp3';
-                        
-                        // 更新歌曲信息
-                        $('#j-link').val('https://music.163.com/#/song?id=' + song.id);
-                        $('#j-link-btn').attr('href', 'https://music.163.com/#/song?id=' + song.id);
-                        $('#j-src').val(audioUrl);
-                        $('#j-src-btn').attr('href', audioUrl);
-                        $('#j-songid').val(song.id);
-                        $('#j-name').val(song.name);
-                        $('#j-author').val(artists);
-                    }
+                    updateCurrentSongInfo();
                 }
             });
 
